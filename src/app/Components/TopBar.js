29fa420fb314581b0191982a9351d1b518037e0a
lakshmijeybe97@gmail.com <lakshmijeybe97@gmail.com>
2024-05-27 15:59:40 -0400

    Navbar color change effects

--- src/app/Components/TopBar.js
+++ src/app/Components/TopBar.js
@@ -1,10 +1,12 @@
-import React from "react";
+"use client";
+import React, { useState } from "react";
 import { HiOutlineSearch } from "react-icons/hi";
 import { HiMiniAtSymbol } from "react-icons/hi2";
 import { FaBell } from "react-icons/fa";
 import Image from "next/image";
 import Link from "next/link";
 const TopBar = () => {
+  const [navSelected, setNavSelected] = useState("");
   return (
     <div className="h-16 bg-slate-100 w-full flex items-center justify-between px-2 fixed top-0">
       <div className="flex gap-2">
@@ -18,8 +20,8 @@ const TopBar = () => {
           className="h-16"
         >
           <path
-            fill-rule="evenodd"
-            clip-rule="evenodd"
+            fillRule="evenodd"
+            clipRule="evenodd"
             d="M3 5C3 3.89543 3.89543 3 5 3H19C20.1046 3 21 3.89543 21 5V19C21 20.1046 20.1046 21 19 21H5C3.89543 21 3 20.1046 3 19V5ZM5 6C5 5.44772 5.44772 5 6 5H10C10.5523 5 11 5.44772 11 6V16C11 16.5523 10.5523 17 10 17H6C5.44772 17 5 16.5523 5 16V6ZM14 5C13.4477 5 13 5.44772 13 6V12C13 12.5523 13.4477 13 14 13H18C18.5523 13 19 12.5523 19 12V6C19 5.44772 18.5523 5 18 5H14Z"
             fill="currentColor"
           ></path>
@@ -33,11 +35,20 @@ const TopBar = () => {
         <div className="flex gap-4">
           <Link
             href="/"
-            className="mt-5 ml-3 hover:text-blue-400 bg-transparent"
+            className={`mt-5 ml-3 hover:text-blue-400 bg-transparent ${
+              navSelected === "Workspaces" ? "text-blue-300" : ""
+            }`}
+            onClick={() => setNavSelected("Workspaces")}
           >
             Workspaces
           </Link>
-          <Link href="/" className="mt-5 hover:text-blue-400 bg-transparent">
+          <Link
+            href="/"
+            className={`mt-5 hover:text-blue-400 bg-transparent ${
+              navSelected === "Create" ? "text-blue-300" : ""
+            }`}
+            onClick={() => setNavSelected("Create")}
+          >
             Create
           </Link>
         </div>
